9a8667841a353fbb58680433a6856dc55306e5c8
ridathariq <153930862+ridathariq@users.noreply.github.com>
2026-07-10 15:13:57 +0530

    Add files via upload

---
+++ transcript.py
@@ -0,0 +1,11 @@
+def transcript():
+   
+   script = 0
+    print("enter url here")
+    if script == valid:
+
+        script = convert to transcript
+        print(script)
+    else: 
+        print("enter valid url")
+    
